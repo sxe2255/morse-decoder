9c2a0f3d3f02cadd43d77ff39957b4cce5cd2e0d
unknown <sxe2255@gmail.com>
2019-10-02 11:51:35 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -38,7 +38,29 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let answer = "";
+    let strSort = expr.match(/.{1,10}/g);
+    let z ="";
+    strSort.forEach((x)=>{
+        if(x === "**********"){
+            z = z + " ";
+        }
+        let s = x.match(/.{1,2}/g);
+        s.forEach((x)=> {
+            if (x === "10"){
+                z = z+ ".";
+            }
+            else if(x === "11"){
+                z = z + "-";
+        }
+
+        })
+        if(z === " "){
+            answer = answer + " ";
+        } else {answer = answer + MORSE_TABLE[z]};
+    z="";    
+    })
+    return answer;
 }
 
 module.exports = {
